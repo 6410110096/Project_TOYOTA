55e5929a3c602753043e523696aeefade101b6ba
6410110096 <flimo2546@gmail.com>
2024-01-28 16:54:19 +0700

    Update Responsive HomeScrenn & Animation HomeScreen

--- lib/features/Screens/Home/home_page.dart
+++ lib/features/Screens/Home/home_page.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
 import 'package:evcar/component/background.dart';
 import 'package:evcar/constants/colors.dart';
-import 'package:evcar/constants/sizes.dart';
+import 'package:evcar/constants/image_strings.dart';
 import 'package:flutter/material.dart';
-import '../../../constants/image_strings.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({Key? key}) : super(key: key);
@@ -23,10 +22,9 @@ class _HomescreenState extends State<Homescreen> {
   @override
   void initState() {
     super.initState();
-    // Initialize the list with empty strings
     separatedValues = ['', '', ''];
-
     fetchData();
+    // Schedule periodic data fetching every 10 seconds
     Future.delayed(const Duration(seconds: 10), fetchData);
   }
 
@@ -34,7 +32,8 @@ class _HomescreenState extends State<Homescreen> {
     try {
       final response = await http.get(
         Uri.parse(
-            'https://api.thingspeak.com/channels/$channelId/feeds.json?api_key=$apiKey&results=1'),
+          'https://api.thingspeak.com/channels/$channelId/feeds.json?api_key=$apiKey&results=1',
+        ),
       );
 
       if (response.statusCode == 200) {
@@ -42,14 +41,11 @@ class _HomescreenState extends State<Homescreen> {
 
         if (jsonResponse['feeds'] != null && jsonResponse['feeds'].isNotEmpty) {
           String field1 = jsonResponse['feeds'][0]['field1'] ?? '';
-          // Update the state with the original field1 value
           setState(() {
             field1Value = field1;
           });
 
-          // Split the field1 value into a list of separated values
           List<String> values = field1.split(' ');
-          // Update the state with the separated values
           setState(() {
             separatedValues = values;
           });
@@ -58,7 +54,8 @@ class _HomescreenState extends State<Homescreen> {
         }
       } else {
         throw Exception(
-            'Failed to load data. Status code: ${response.statusCode}');
+          'Failed to load data. Status code: ${response.statusCode}',
+        );
       }
     } catch (e) {
       debugPrint('Error: $e');
@@ -68,176 +65,144 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Background(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Material(
-                  type: MaterialType.transparency,
-                  child: Row(),
-                ),
-                const Padding(padding: EdgeInsets.all(tDefaultSpace)),
-                const Text(
-                  'TOYOTA EV & PSU',
-                  style: TextStyle(
-                      fontSize: 33,
-                      fontWeight: FontWeight.w600,
-                      color: tPrimaryColor),
-                ),
-
-                const Text(
-                  'Model: CoE33 & EE55',
-                  style: TextStyle(fontSize: 16, color: tPrimaryColor),
-                ),
-                const Text(
-                  'Hello PSU, Welcome to EV Car model Coe & EE',
-                  style: TextStyle(fontSize: 12, color: tPrimaryColor),
-                ),
-                const SizedBox(height: 15),
-                Image.asset(tWelcomeScreenImage, width: 300, height: 300),
-                const SizedBox(height: 15),
-
-                // Update the Text widgets with the fetched values
-                //Text('Value 1: ${separatedValues.length > 0 ? separatedValues[0] : ''}'),
-                //Text('Value 2: ${separatedValues.length > 1 ? separatedValues[1] : ''}'),
-                //Text('Value 3: ${separatedValues.length > 2 ? separatedValues[2] : ''}'),
-
-                //แสดงค่าแบตเตอรี่ ในรูปแบบ Charging
-                CircularPercentIndicator(
-                  radius: 100.0,
-                  lineWidth: 25.0,
-                  animation: true,
-                  percent: separatedValues.isNotEmpty
-                      ? (double.tryParse(separatedValues[0]) ?? 0.0) / 100.0
-                      : 0.0,
-                  center: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "${(separatedValues.isNotEmpty ? double.tryParse(separatedValues[0]) ?? 0.0 : 0.0) * 1}%",
-                          style: const TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w300,
-                            color: tPrimaryColor,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  circularStrokeCap: CircularStrokeCap.round,
-                  progressColor: Colors.red[600],
-                ),
-                const Row(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+            ),
+            const Text(
+              'TOYOTA EV & PSU',
+              style: TextStyle(
+                fontSize: 33,
+                fontWeight: FontWeight.w600,
+                color: tPrimaryColor,
+              ),
+            ),
+            const Text(
+              'Model: CoE33 & EE55',
+              style: TextStyle(fontSize: 16, color: tPrimaryColor),
+            ),
+            const Text(
+              'Hello PSU, Welcome to EV Car model Coe & EE',
+              style: TextStyle(fontSize: 12, color: tPrimaryColor),
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              tWelcomeScreenImage,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 20),
+            CircularPercentIndicator(
+              radius: 100.0,
+              lineWidth: 25.0,
+              animation: true,
+              percent: separatedValues.length > 0
+                  ? (double.tryParse(separatedValues[0]) ?? 0.0) / 100.0
+                  : 0.0,
+              center: Center(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Text(
-                        'Battery',
-                        style: TextStyle(fontSize: 15, color: tPrimaryColor),
+                    Text(
+                      "${(separatedValues.length > 0 ? double.tryParse(separatedValues[0]) ?? 0.0 : 0.0) * 1}%",
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w300,
+                        color: tPrimaryColor,
                       ),
-                    )
+                    ),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      // Temp
-                      Card(
-                        color: tPrimaryColor,
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: SizedBox(
-                            width: 150,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Temp',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: tPrimaryColor,
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Center(
-                                  child: Text(
-                                    "${(separatedValues.isNotEmpty ? double.tryParse(separatedValues[1]) ?? 0.0 : 0.0) * 1} °C",
-                                    style: const TextStyle(
-                                      fontSize: 50,
-                                      fontWeight: FontWeight.bold,
-                                      color: tPrimaryColor,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Card(
-                        color: tPrimaryColor,
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: SizedBox(
-                            width: 150,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Voltage',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: tPrimaryColor,
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Center(
-                                  child: Text(
-                                    "${(separatedValues.isNotEmpty ? double.tryParse(separatedValues[2]) ?? 0.0 : 0.0) * 1} V",
-                                    style: const TextStyle(
-                                      fontSize: 50,
-                                      fontWeight: FontWeight.bold,
-                                      color: tPrimaryColor,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                    ],
+              ),
+              circularStrokeCap: CircularStrokeCap.round,
+              progressColor: Colors.red[600],
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Battery',
+              style: TextStyle(fontSize: 15, color: tPrimaryColor),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TempVoltageCard(
+                    label: 'Temp',
+                    value: separatedValues.length > 0
+                        ? double.tryParse(separatedValues[1]) ?? 0.0
+                        : 0.0,
+                    unit: '°C',
+                  ),
+                  const SizedBox(width: 20),
+                  TempVoltageCard(
+                    label: 'Voltage',
+                    value: separatedValues.length > 0
+                        ? double.tryParse(separatedValues[2]) ?? 0.0
+                        : 0.0,
+                    unit: 'V',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TempVoltageCard extends StatelessWidget {
+  final String label;
+  final double value;
+  final String unit;
+
+  const TempVoltageCard({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.unit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: tDarkColor,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SizedBox(
+          width: 150,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                label,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: tPrimaryColor,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Center(
+                child: Text(
+                  "${value * 1} $unit",
+                  style: const TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: tPrimaryColor,
                   ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 10),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
